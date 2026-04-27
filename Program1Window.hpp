61b9d8686b5cd9541a2077b83caf14e70b7699fd
Julia <yuliya.sandu26@gmail.com>
2026-04-27 10:40:18 +0200

    Update Program1Window.hpp

--- Program1Window.hpp
+++ Program1Window.hpp
@@ -1,1 +1,38 @@
+#ifndef PROGRAM1WINDOW_HPP
+#define PROGRAM1WINDOW_HPP
 
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QSpinBox>
+#include <QPushButton>
+#include <QLabel>
+
+class Program1Window : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit Program1Window(QWidget *parent = nullptr);
+
+private slots:
+    void onSelectJsonFile();
+    void onLoadFromTxt();
+    void onSaveObject();
+
+private:
+    void appendToJsonFile(const InkObject &obj);
+    void loadJsonArray();
+
+    QLineEdit *m_nameEdit;
+    QLineEdit *m_descEdit;
+    QLineEdit *m_colorEdit;
+    QSpinBox  *m_inkSpin;
+    QPushButton *m_selectJsonBtn;
+    QPushButton *m_loadTxtBtn;
+    QPushButton *m_saveBtn;
+    QLabel *m_jsonFileLabel;
+
+    QString m_currentJsonPath;
+    QJsonArray m_existingArray;
+};
+
+#endif // PROGRAM1WINDOW_HPP
